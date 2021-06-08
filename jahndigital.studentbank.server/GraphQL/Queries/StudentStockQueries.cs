8803e03811e3e629499a2b9005f2ed99a1789405
Justin Jahn <justin@justinjahn.me>
2021-06-07 21:20:49 -0700

    Student stock history, minor tweaks.

--- jahndigital.studentbank.server/GraphQL/Queries/StudentStockQueries.cs
+++ jahndigital.studentbank.server/GraphQL/Queries/StudentStockQueries.cs
@@ -1,10 +1,12 @@
 using System.Linq;
+using System.Threading.Tasks;
 using HotChocolate;
 using HotChocolate.AspNetCore.Authorization;
 using HotChocolate.Resolvers;
 using HotChocolate.Types;
 using HotChocolate.Types.Relay;
 using jahndigital.studentbank.dal.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace jahndigital.studentbank.server.GraphQL.Queries
 {
@@ -12,21 +14,58 @@ namespace jahndigital.studentbank.server.GraphQL.Queries
     public class StudentStockQueries
     {
         /// <summary>
-        /// Get the stocks purchased by the user.
+        /// Get the stocks purchased by the student specified.
         /// </summary>
+        /// <param name="studentId"></param>
         /// <param name="context"></param>
         /// <param name="resolverContext"></param>
         /// <returns></returns>
         [UsePaging, UseSelection, UseSorting, UseFiltering, Authorize]
-        public IQueryable<dal.Entities.StudentStock> GetStudentStocks(
+        public async Task<IQueryable<dal.Entities.StudentStock>> GetStudentStocksAsync(
+            long studentId,
             [Service] AppDbContext context,
             [Service] IResolverContext resolverContext
         ) {
             var id = resolverContext.GetUserId() ?? throw ErrorFactory.NotFound();
             var type = resolverContext.GetUserType() ?? throw ErrorFactory.NotFound();
-            resolverContext.SetUser(id, type);
-            if (type == Constants.UserType.User) return context.StudentStocks;
-            return context.StudentStocks.Where(x => x.StudentId == id);
+            resolverContext.SetUser(studentId, type);
+
+            var auth = await resolverContext
+                .AuthorizeAsync($"{Constants.AuthPolicy.DataOwner}<{Constants.Privilege.ManageStocks}>");
+
+            if (!auth.Succeeded) throw ErrorFactory.Unauthorized();
+
+            return context.StudentStocks.Where(x => x.StudentId == studentId);
+        }
+
+        /// <summary>
+        /// Get the purchase history for a student's stock.
+        /// </summary>
+        /// <param name="studentStockId"></param>
+        /// <param name="context"></param>
+        /// <param name="resolverContext"></param>
+        /// <returns></returns>
+        [UsePaging, UseSelection, UseSorting, UseFiltering, Authorize]
+        public async Task<IQueryable<dal.Entities.StudentStockHistory>> GetStudentStockHistoryAsync(
+            long studentStockId,
+            [Service] AppDbContext context,
+            [Service] IResolverContext resolverContext
+        ) {
+            var type = resolverContext.GetUserType() ?? throw ErrorFactory.NotFound();
+
+            var studentStock = await context.StudentStocks
+                .Where(x => x.Id == studentStockId)
+                .FirstOrDefaultAsync()
+            ?? throw ErrorFactory.NotFound();
+
+            resolverContext.SetUser(studentStock.StudentId, type);
+
+            var auth = await resolverContext
+                .AuthorizeAsync($"{Constants.AuthPolicy.DataOwner}<{Constants.Privilege.ManageStocks}>");
+
+            if (!auth.Succeeded) throw ErrorFactory.Unauthorized();
+
+            return context.StudentStockHistory.Where(x => x.StudentStockId == studentStockId);
         }
     }
 }
