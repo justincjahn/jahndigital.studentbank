50d3f4ffe60d02af6b6f53f1301f2fb281edc541
Justin Jahn <justin@justinjahn.me>
2022-05-26 23:37:29 -0700

    Migrated Share mutations to CQRS.

---
+++ src/Application/Shares/Commands/NewShare/NewShareCommand.cs
@@ -0,0 +1,58 @@
+﻿using JahnDigital.StudentBank.Application.Common.Exceptions;
+using JahnDigital.StudentBank.Application.Common.Interfaces;
+using JahnDigital.StudentBank.Domain.Entities;
+using JahnDigital.StudentBank.Domain.ValueObjects;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace JahnDigital.StudentBank.Application.Shares.Commands.NewShare;
+
+/// <summary>
+/// Create a new share.
+/// </summary>
+/// <param name="StudentId"></param>
+/// <param name="ShareTypeId"></param>
+/// <param name="InitialBalance"></param>
+public record NewShareCommand(long StudentId, long ShareTypeId, Money InitialBalance) : IRequest<long>;
+
+public class NewShareCommandHandler : IRequestHandler<NewShareCommand, long>
+{
+    private IAppDbContext _context;
+
+    public NewShareCommandHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<long> Handle(NewShareCommand request, CancellationToken cancellationToken)
+    {
+        Student student = await _context.Students
+            .Include(x => x.Group)
+            .ThenInclude(x => x.Instance)
+            .ThenInclude(x => x.ShareTypeInstances)
+            .Where(x => x.Id == request.StudentId)
+            .FirstOrDefaultAsync(cancellationToken)
+        ?? throw new NotFoundException(nameof(Student), request.StudentId);
+
+        var hasShareType = student.Group.Instance.ShareTypeInstances.Any(x => x.ShareTypeId == request.ShareTypeId);
+
+        if (!hasShareType)
+        {
+            throw new InvalidOperationException(
+                $"Student {student.AccountNumber} in instance {student.Group.Instance.Description} does not have access to share type id {request.ShareTypeId}"
+            );
+        }
+
+        var share = new Share
+        {
+            StudentId = request.StudentId,
+            ShareTypeId = request.ShareTypeId,
+            DateLastActive = DateTime.UtcNow,
+            Balance = request.InitialBalance
+        };
+
+        _context.Shares.Add(share);
+        await _context.SaveChangesAsync(cancellationToken);
+        return share.Id;
+    }
+}
---
+++ src/Application/Shares/Commands/RestoreShare/RestoreShareCommand.cs
@@ -0,0 +1,29 @@
+﻿using JahnDigital.StudentBank.Application.Common.Exceptions;
+using JahnDigital.StudentBank.Application.Common.Interfaces;
+using JahnDigital.StudentBank.Domain.Entities;
+using MediatR;
+
+namespace JahnDigital.StudentBank.Application.Shares.Commands.RestoreShare;
+
+public record RestoreShareCommand(long ShareId) : IRequest;
+
+public class RestoreShareCommandHandler : IRequestHandler<RestoreShareCommand>
+{
+    private readonly IAppDbContext _context;
+
+    public RestoreShareCommandHandler(IAppDbContext context) {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(RestoreShareCommand request, CancellationToken cancellationToken)
+    {
+        var share = await _context.Shares.FindAsync(new object?[] { request.ShareId }, cancellationToken)
+            ?? throw new NotFoundException(nameof(Share), request.ShareId);
+
+        share.DateDeleted = null;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;;
+    }
+}
