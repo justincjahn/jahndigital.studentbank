90f623684c4de7b11e298dba6b3bb00eeeed9345
Justin Jahn <justin@justinjahn.me>
2022-06-04 14:55:17 -0700

    Finished student mutations conversion to CQRS.  Added new ErrorFactory methods.

---
+++ src/Application/Students/Commands/RestoreStudent/RestoreStudentCommand.cs
@@ -0,0 +1,33 @@
+﻿using JahnDigital.StudentBank.Application.Common.Exceptions;
+using JahnDigital.StudentBank.Application.Common.Interfaces;
+using MediatR;
+
+namespace JahnDigital.StudentBank.Application.Students.Commands.RestoreStudent;
+
+public record RestoreStudentCommand(long Id) : IRequest;
+
+public class RestoreStudentCommandHandler : IRequestHandler<RestoreStudentCommand>
+{
+    private readonly IAppDbContext _context;
+
+    public RestoreStudentCommandHandler(IAppDbContext context) {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(RestoreStudentCommand request, CancellationToken cancellationToken)
+    {
+        var student = await _context.Students.FindAsync(new object?[] { request.Id }, cancellationToken)
+            ?? throw new StudentNotFoundException(request.Id);
+
+        if (student.DateDeleted is null)
+        {
+            throw new InvalidOperationException($"Student {request.Id} is not deleted.");
+        }
+
+        student.DateDeleted = null;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
